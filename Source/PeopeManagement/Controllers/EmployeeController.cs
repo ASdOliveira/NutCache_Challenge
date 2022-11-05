a84f049b665a1ab076d312b5e08bbbb03e5d1506
ASdOliveira <arysson.silva@hotmail.com>
2022-11-05 11:22:15 -0300

    Initial version of Employee API

---
+++ Source/PeopeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopeManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+
+        [HttpGet]
+        public Employee Get()
+        {
+            return new Employee();
+        }
+    }
+}
---
+++ Source/PeopeManagement/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopeManagement.Models
+{
+    public enum Genders
+    {
+        MALE,
+        FEMALE,
+        UNINFORMED
+    }
+
+    public enum Teams
+    {
+        NONE,
+        MOBILE,
+        FRONTEND,
+        BACKEND
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; } //required
+        public DateTime BirthDate { get; set; } // required
+        public Genders Gender { get; set; } //required
+        public string Email { get; set; } //required
+        public string CPF { get; set; } //required
+        public DateTime StartDate { get; set; }
+        public Teams Team { get; set; }
+    }
+}
